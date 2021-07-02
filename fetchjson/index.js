7ef801822b6f7d5b510d8d4087e10936757d06c2
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-07-02 12:23:36 -0400

    applied TS to fetchjson

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,5 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (res) {
-    console.log(res.data);
+    
 });
